d05168dbddb4dc08be8ffb5ad599f6a60ae97e2f
JoJettison <jb9013xz@gmail.com>
2018-11-11 15:27:16 -0600

    Implemented 1st part of page insertion

--- page-insert.php
+++ page-insert.php
@@ -5,10 +5,14 @@
 function generatePaID() {
   $idfile = new DOMDocument();
   $idfile->load('id.xml');
+  if ($idfile === FALSE) {
+    echo "database key file missing, error";
+    exit();
+  }
   $idroot = $idfile->documentElement;
   $ID = $idroot->getElementsByTagName('pageid')->item(0)->textContent;
   ++$ID;
-  $idroot->getElementsByTagName('pageid')->item(0)->textContent = ID
+  $idroot->getElementsByTagName('pageid')->item(0)->textContent = $ID;
   $idfile->save('id.xml');
   return $ID;
 }
@@ -20,15 +24,22 @@ if ($BrunoCONN->connect_error) {
 }
   //Storing data from the form into variables
   $PageID = generatePaID();
-  $Name = '';
-  $Description = '';
-  $Category = '';
-  $Image = '';
+  $Name = $_POST['pageName'];
+  $Description = $_POST['description'];
+  $Category = $_POST['category'];
+
+  /*if(isset($_POST["submit"])){
+    $check =getimagesize($_FILES["image"]["tmp_name"]);
+    if($check !== FALSE)
+    $Image =$_FILES['image']['tmp_name'];
+    $imgContent =addslashes(file_get_contents($Image));
+  }*/
+  $Image = 'alpineMountain.png';
 
 
   //Create query to generate new page
-  $PageInsert = "INSERT INTO Page(PageID,Name,Description,Category,Image)
-  VALUES('$PageID','$Name','$Description','$Category','$Image')";
+  $PageInsert = "INSERT INTO Page(PageID,Name,Description,Category,Image,CreatorID)
+  VALUES('$PageID','$Name','$Description','$Category','$Image',NULL)";
 
   //Execute generated query
   if($BrunoCONN->query($PageInsert) === TRUE){
