1f25d2f030e0f3cf650589cb6c3094579c04dd09
JoJettison <jb9013xz@gmail.com>
2018-11-15 16:05:23 -0600

    Base logic for grabbing pginfo and getting posts on a page

--- page-info.php
+++ page-info.php
@@ -8,10 +8,10 @@ require_once 'bruno-config.php';
 
 $pageID = $_SESSION['PageID'];
 
-$PageInfoSQL = "SELECT * FROM PAGE WHERE PageID = ?"
+$PageInfoSQL = "SELECT * FROM PAGE WHERE PageID = ?";
 //Prepare query statement
 if($GetPageInfo =$BrunoCONN->prepare($PageInfoSQL)){
-  $GetPageInfo->bind_param(s,$ParamPgID);
+  $GetPageInfo->bind_param("s",$ParamPgID);
   //Designate variable for pageid binding
   $ParamPgID = $pageID;
   //Actually pass the username into the bound variable
@@ -19,9 +19,22 @@ if($GetPageInfo =$BrunoCONN->prepare($PageInfoSQL)){
   //Execute query
   $GetUserInfo->store_result();
   //Store result of query
-  if($GetPageInfo->num_rows== 1){
+    if($GetPageInfo->num_rows== 1){
     //See if page found in the database
-    $GetPageInfo->bind_result($pageID,$pgName,$pgDesc,$pgCat,$pgImg,$pgCreatorID)
+    $GetPageInfo->bind_result($pageID,$pgName,$pgDesc,$pgCat,$pgImg,$pgCreatorID);
+    }
   }
+}
+
+$PagePostsSQL = "SELECT * FROM POST WHERE PageID =?"
+if($GetPosts =$BrunoCONN->prepare($PagePostsSQL)){
+  $GetPosts->bind_param("s",$ParamPgID);
+  $ParamPgID = $pageID;
+  if($GetPosts->execute()){
+      $GetPosts->store_result();
+      if($GetPosts->num_rows ==1){
+        $GetPageInfo->bind_result($poID,$prID,$paID);
+      }
+    
   }
 }
