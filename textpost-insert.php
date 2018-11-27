55de5d0900de46fe0fcbe57fba7e377a7ca089fe
JoJettison <jb9013xz@gmail.com>
2018-11-27 11:00:40 -0600

    Added textposting

---
+++ textpost-insert.php
@@ -0,0 +1,58 @@
+<?php
+require_once 'bruno-config.php';
+
+function generatePoID($mode) {
+  $idfile = new DOMDocument();
+  $idfile->load('id.xml');
+  if ($idfile === FALSE) {
+    echo "database key file missing, error";
+    exit();
+  }
+  $idroot = $idfile->documentElement;
+  $ID = $idroot->getElementsByTagName('postid')->item(0)->textContent;
+  if($mode === 0){
+    ++$ID;
+  }
+  else{
+    --$ID;
+  }
+  $idroot->getElementsByTagName('postid')->item(0)->textContent = $ID;
+  $idfile->save('id.xml');
+  return $ID;
+}
+
+require_once 'get-profileid.php';
+
+$TContent = $_POST['TContent'];
+$PostID = generatePoID(0);
+$CreationDate = date('Y-m-d H:i:s');
+
+$PostInsertProfile =" INSERT INTO Post(PostID, ProfileID, PageID, CreationDate)
+VALUES('$PostID','$PrID', NULL,'$CreationDate')";
+//
+$TextInsert =" INSERT INTO Text(PostID, TContent)
+VALUES('$PostID','$Content')";
+
+if($BrunoCONN->query($PostInsertProfile) === TRUE){
+
+  if($BrunoCONN->query($TextInsert) === TRUE){
+    echo "New post generated in database";
+    header("Location: http://localhost/BRUNO/profilehome.html");
+    $BrunoCONN->close();
+  }else{
+    $poerror = "Error: ".$TextInsert."<br>". $BrunoCONN->error;
+    $_SESSION['Error'] = $poerror;
+    header("Location: http://localhost/BRUNO/error.php");
+    $BrunoCONN->close();
+
+  }
+}
+else{
+    $poerror = "Error: ".$PostInsertProfile. "<br>". $BrunoCONN->error;
+    $_SESSION['Error'] = $poerror;
+    header("Location: http://localhost/BRUNO/error.php");
+    generatePoID(-1);
+    $BrunoCONN->close();
+  }
+
+ ?>
