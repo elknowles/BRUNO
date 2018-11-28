547b2e51a1a41e0e6eae893ec534bfa5d70dbee1
JoJettison <jb9013xz@gmail.com>
2018-11-27 19:51:40 -0600

    Implelmented message sending

---
+++ message-insert.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+?>
+<html>
+<body>
+<?php
+
+require_once "bruno-config.php";
+
+function generateMsgID($mode) {
+  $idfile = new DOMDocument();
+  $idfile->load('id.xml');
+  if ($idfile === FALSE) {
+    echo "database key file missing, error";
+    exit();
+  }
+  $idroot = $idfile->documentElement;
+  $ID = $idroot->getElementsByTagName('msgid')->item(0)->textContent;
+  if($mode === 0){
+    ++$ID;
+  }
+  else{
+    --$ID;
+  }
+  $idroot->getElementsByTagName('msgid')->item(0)->textContent = $ID;
+  $idfile->save('id.xml');
+  return $ID;
+}
+
+$MessageID = generateMsgID(0);
+$CreationDate = date('Y-m-d H:i:s');
+$Content = $_POST['TContent'];
+$RecipUsername = $_SESSION['RecipUsr'];
+
+$RecipientIDSQL = "SELECT ProfileID FROM Profile WHERE Username =?";
+if($GetUserInfo = $BrunoCONN->prepare($RecipientIDSQL)){
+	  $GetUserInfo->bind_param("s",$ParamUsr);
+	  //Designate variable for username binding
+	  $ParamUsr =$RecipUsername;
+	  //Actually pass the username into the bound variable
+	  if($GetUserInfo->execute()){
+	    //Execute query
+	      $GetUserInfo->store_result();
+	      //Store result of query
+	      if($GetUserInfo->num_rows == 1){
+	        //See if username found in database
+	        $GetUserInfo->bind_result($RecipPrID);
+          if($GetUserInfo->fetch()){
+            //RecipientID stored in $RecipPrID variable
+          }
+	      }else {
+	        $lgerror = 'Username not located in database';
+	        $_SESSION['Error'] =$lgerror;
+          generateMsgID(-1);
+	      }
+	  }
+	}
+
+  require_once "get-profileid.php";
+
+$InsertMessage =" INSERT INTO Message(MessageID, SenderID, RecipientID, `TimeStamp`, `Text`)
+VALUES('$MessageID','$PrID','$RecipPrID','$CreationDate','$Content')";
+
+if($BrunoCONN->query($InsertMessage) === TRUE){
+  echo "message sent to ",$RecipUsername. "<br>";
+  $BrunoCONN->close();
+}else{
+  $_SESSION['Error'] = "Error: ".$InsertMessage. "<br>". $BrunoCONN->error;
+  header("Location: http://localhost/BRUNO/error.php");
+  $BrunoCONN->close();
+  generateMsgID(-1);
+}
+?>
+</body>
+</html>
--- messages.php
+++ messages.php
@@ -4,34 +4,10 @@ session_start();
 <?php
 	require_once "bruno-config.php";
 
-	$Recipient = $_POST['Recipient'];
-	$_SESSION['Recipient'] =$Recipient;
-	echo $Recipient;
-	$RecipientIDSQL = "SELECT ProfileID FROM Profile WHERE Username =?";
-
-	if($GetUserInfo = $BrunoCONN->prepare($UserInfoSQL)){
-	  $GetUserInfo->bind_param("s",$ParamUsr);
-	  //Designate variable for username binding
-	  $ParamUsr =$Username;
-	  //Actually pass the username into the bound variable
-	  if($GetUserInfo->execute()){
-	    //Execute query
-	      $GetUserInfo->store_result();
-	      //Store result of query
-	      if($GetUserInfo->num_rows == 1){
-	        //See if username found in database
-	        $GetUserInfo->bind_result($Username,$AvatarFile);
-	        header("sendMessage.php");
-	      }else {
-	        $lgerror = 'Username not located in database';
-	        $_SESSION['Error'] =$lgerror;
-	      }
-	  }
-	}
-	$PostInsertMessage =" INSERT INTO Message(MessageID, SenderID, RecipientID, TimeStamp)
-  	VALUES('$MessageID','$PrID', NULL,'$CreationDate')";
-
-$_POST['recipent'];
+	//$Recipient = $_POST['Recipient'];
+	//$_SESSION['Recipient'] =$Recipient;
+	//echo $Recipient;
+	//$_POST['recipent'];
 
 
 ?>
@@ -58,16 +34,16 @@ $_POST['recipent'];
 <body>
 	<section class="hero">
 		<div class="hero-inner">
-			<form id="recipentSelection" method="post">
+			<form id="recipentSelection" action="sendMessage.php" method="post">
 				<h1>MESSAGES</h1>
 				<h3>Select the recipient</h3>
 				<tr>
 					<td>Recipient</td>
 					<td>
 						<select name="recipient" id="recipient" style="color: black" value="">
-						<?php 
+						<?php
 						require_once 'bruno-config.php';
-							$sql = mysqli_query($BrunoCONN, "SELECT P.Username FROM Profile as P WHERE Privacy = '1'");
+  							$sql = mysqli_query($BrunoCONN, "SELECT P.Username FROM Profile as P WHERE Privacy = '1'");
 							while ($row = $sql->fetch_assoc()){
 							echo "<option>" . $row['Username'] . "</option>";
 							}
@@ -82,7 +58,7 @@ $_POST['recipent'];
 					<td>
 						<form>
 							<label for="username"><b>Enter the Recipient's Username:</b></label>
-							<input type="text" placeholder="Enter Username" name="recipient" minlength="8" maxlength="12" required="1">
+							<input type="text" placeholder="Enter Username" name="recipient" minlength="8" maxlength="12">
 							<input type="submit" value="Send" style="width: 128px; height: 128px; border-radius: 50%;">
 						</form>
 					</td>
